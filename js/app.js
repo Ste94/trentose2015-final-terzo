dd671defabe3f1df66ce4dc1a7095f4c73ec3706
Stefano <stefano__1994@hotmail.it>
2016-02-10 15:48:20 +0100

    modificato app.js e app.spec.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ var ParlaModel = {
     * dictionaries 
     */
    langs : languages, 
+    
+    
   
    /* Returns the score of the given phrase in the given language
     * The score is computed simply as the number of words - in the
@@ -14,6 +16,22 @@ var ParlaModel = {
    getLanguageScore : function (phrase, language){
      // write your code here, but don't add any additional 
      // parameters to the the function
+       
+       if(ParlaModel.langs.indexOf(language) != -1){
+           
+            var dictionary = ParlaModel.langs[ParlaModel.langs.indexOf(language)].dictionary;
+           
+            console.log(dictionary); 
+           
+            var res = 0;
+           
+            var parole = phrase.split(" ");
+            
+            for (var i = 0; i<parole.length; i++){
+                
+            } 
+           
+       } else return null;
    },
   
    /* Returns the name of the language in which the phrase
@@ -23,6 +41,36 @@ var ParlaModel = {
    detectLanguage : function (phrase) {
      // write your code here, but don't add any additional 
      // parameters to the the function     
-   }
+       
+       var e, i, s;
+       e = getlanguageScore(phrase, "English");
+       i = getLanguageScore(phrase, "Italian");
+       s = getLanguageScore(phrase, "Spanish");
+       
+        if ((e>=i) && (e>=s)) {
+            return "English";
+        } else if ((i>e) && (i>=s))  {
+            return "Italian";
+        } else if ((s>e) && (s>i)) {
+            return "Spanish";
+        }
+        
+    }
+       
+   
          
 };
+
+var View = {
+    
+};
+
+var Octopus = {
+    
+};
+
+$(document).ready(function(){
+   console.log(ParlaModel.getLanguageScore("the best","Italian"));
+    //Octopus.init();
+});
+
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -2,9 +2,33 @@
 /* Try to maximise the coverage of the ParlaModel object */
 
 describe("Parla", function() {
+    describe("getLanguageScore", function() {
 
-  it("should...", function() {  
+        it("should properly return null if the language is not valid", function() {  
+            expect(Parla.getLanguageScore("the best","aka")).toBe(null);
+        }); 
+        
+        it("should properly return the number of matches", function() {  
+            expect(Parla.getLangaugeScore("the best","English")).toBe(1);
+            expect(Parla.getLanguageScore("the best","Italian")).toBe(0);
+        }); 
+        
+        
+    }); 
     
-  });    
+    describe("detectLanguage", function() {
+
+        it("should properly return the right language", function() {  
+            expect(Parla.detectLanguage("the best").toBe("English");
+            expect(Parla.detectLanguage("il migliore").toBe("Italian");
+            expect(Parla.detectLanguage("el mejo").toBe("Spanish");
+        }); 
+    }); 
+ 
  
 });
+
+
+ 
+ 
+
